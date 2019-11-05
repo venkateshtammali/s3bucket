15a7c308829a9a2db81d9e089e3ef752972b0adf
venkateshtammali <39961342+venkateshtammali@users.noreply.github.com>
2019-11-05 11:55:55 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,14 @@
+# main.tf
+terraform {
+  required_version = "~> 0.12.0"
+
+  backend "remote" {}
+}
+provider "aws" {
+    access_key = "${var.AWS_ACCESS_KEY}"
+    secret_key = "${var.AWS_SECRET_KEY}"
+    region = "${var.AWS_REGION}"
+}
 provider "aws" {
   region = "ap-south-1"
 }
